5d8ccbbc59031b29c8691b20781a22d6a28513bd
Ingyu Woo <iglenwoo@gmail.com>
2019-08-03 16:47:14 -0700

    init project

---
+++ DogDiary/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DogDiary
+//
+//  Created by Ingyu Woo on 8/3/19.
+//  Copyright © 2019 Ingyu Woo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
